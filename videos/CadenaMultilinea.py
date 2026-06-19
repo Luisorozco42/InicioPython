7742b22ec31e695d2627191a6457032807f099f2
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 05:49:25 -0600

    multiple line strings

---
+++ videos/CadenaMultilinea.py
@@ -0,0 +1,23 @@
+# se usan triples comillas simples o dobles
+
+menu = """
+MENU DEL DIA
+------------
+
+1. Tacos al pastor
+2. Enchiladas verdes
+3. Quesadillas
+"""
+
+print(menu)
+
+#Ejemplo 2 correo
+
+correo = """
+Hola equipo:
+
+Les aviso que ya termine la tarea de python.
+¡El código funciona perfecto!
+
+Saludos cordiales.
+"""
