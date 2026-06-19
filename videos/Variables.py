5e443152568d741aca26440153de39becc172a29
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-18 18:08:15 -0600

    variables

---
+++ videos/Variables.py
@@ -0,0 +1,10 @@
+#En este momento vamos a guardar los datos en variables
+nombre = "Ana"
+edad = 22
+pais = "México"
+frase_favorita = "Nunca dejes de aprender"
+
+print("Nombre: ", nombre)
+print("Edad: ", edad)
+print("Pais: ", pais)
+print("Frase favorita: ", frase_favorita)
