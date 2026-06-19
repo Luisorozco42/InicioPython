47ba0399e0cdae31ddc45a2dc02fe572f7b0eb96
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 05:12:53 -0600

    challenge #3

---
+++ retos/TiendaOnline.py
@@ -0,0 +1,25 @@
+# Para este reto es necesario simular una compra en una tienda online
+# Este tiene que tener el nombre del producto comprado
+# El precio, la cant en inventario y si esta disponible
+# Luego despues de crearlo hay que realizar algunos cambios y mostrar ambas versiones en pantalla
+
+nombre_producto = "Cámara digital"
+precio = 399.99
+cantidad_inventario = 20
+esta_dispinble = True
+
+print("Nombre del producto: ", nombre_producto)
+print("Precio del producto: $", precio)
+print("Cantidad Inventario: ", cantidad_inventario)
+print("Disponible: ", esta_dispinble)
+
+# Reasignación
+nombre_producto = "Celular"
+precio = 999.99
+cantidad_inventario = 10
+esta_dispinble = False
+
+print("Nombre del producto: ", nombre_producto)
+print("Precio del producto: $", precio)
+print("Cantidad Inventario: ", cantidad_inventario)
+print("Disponible: ", esta_dispinble)
