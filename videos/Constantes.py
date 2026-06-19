8a7a204c546d100649b305e4cee4704c965b87a5
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 05:25:11 -0600

    leccion de constantes

---
+++ videos/Constantes.py
@@ -0,0 +1,18 @@
+# Constantes en python
+# Aquí no existen en si, solo son un convención
+# en otras palabras solo hacemos como que existen
+import math
+
+print("*** Constantes en Python ***")
+
+PI = 3.14159
+print("El valor de pi es: ", PI)
+
+NOMBRE_BASE_DATOS = 'clientes_db'
+print("Nombre del base de datos: ", NOMBRE_BASE_DATOS)
+
+NOMBRE_BASE_DATOS = "listado_clientes_bd"
+print("No cambiar el nombre de una constante: ", NOMBRE_BASE_DATOS)
+
+# Usar una constante del lenguaje de Python
+print("Valor de math.pi: ", math.pi)
