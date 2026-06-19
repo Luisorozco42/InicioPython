7be29352265c766187f4a0fb78189c223954b7aa
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 05:45:00 -0600

    strings

---
+++ videos/Cadenas.py
@@ -0,0 +1,18 @@
+# Cadenas de caracteres en python
+# se puede con comillas simples
+
+fruta = 'Manzana'
+
+#O con comillas dobles
+
+color = "rojos"
+
+#citar a alguien
+
+cita = 'El maestro dijo: "Estudien Mucho"' #Razones por la cual se pueden hacer ambas formas
+
+#ejemplo: Titulo de un libro
+
+libro = "Lei el libro 'El principito' ayer"
+print(cita)
+print(libro)
