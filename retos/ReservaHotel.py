24323db5f1c8bbcb6f03702e7ed3af642fdec58c
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 04:54:06 -0600

    challenge #2

---
+++ retos/ReservaHotel.py
@@ -0,0 +1,13 @@
+#  El propósito de este reto es guardar los datos de un cliente para la reserva en un hotel
+# Los datos son: Nombre, dias de instancia, tarifa diaria, y si el cuarto tiene vista al mar
+
+nombre = "Laura Martinez"
+dias_instancia = 5
+tarifa_diaria = 1200.0
+tiene_vista_mar = True
+
+print("*** Sistema de Reserva de Hoteles ***")
+print("Nombre: ", nombre)
+print("Dias de instancia: ", dias_instancia)
+print("Tarifa de diaria: ", tarifa_diaria)
+print("¿Tiene vista mar?: ", tiene_vista_mar)
