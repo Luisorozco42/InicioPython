ea51ad7b62e7723403ad1961e0e3a9cadd82af48
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-18 18:51:51 -0600

    from now on, I will use this type of wrritinng variables

--- videos/ModificarVariables.py
+++ videos/ModificarVariables.py
@@ -1,8 +1,10 @@
+#Aparentemente, es buena práctica en python usar el snake case a diferencia de java que es el camelcase
+
 nombre = "El programador nocturno"
 nivelEnergia = 85
 tieneCapa = False
 
-print("=== Identidad Orignal ===")
+print("=== Identidad Original ===")
 print("Nombre: ", nombre)
 print("Nivel Energía: ", nivelEnergia)
 print("¿Tiene capa?: ", tieneCapa)
---
+++ videos/NombresVariables.py
@@ -0,0 +1,18 @@
+#Asignamos valores a las variables
+
+nombre_explorador = "Luna Vega"
+planeta_origen = "Marte"
+edad_explorador = 29
+misiones_completadas = 4
+nivel_energia = 87.5
+
+#for (no se debe usar palabras reservadas)
+NIVEL_ENERGIA = 100.00 #Se considera una constante
+#También se pueden poner nombres con letras minúsculas y si son los mismos, pero difiere en algo son
+#2 variables diferentes
+
+print("=== REGISTRO ESPACIAL ===")
+print("Nombre del explorador: ", nombre_explorador)
+print("Planeta de origen: ", planeta_origen)
+print("Edad: ", edad_explorador)
+print("Nivel Energia: ", nivel_energia)
