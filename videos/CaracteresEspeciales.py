d8df3f94a6c18efc6041e8b7100e321cbe5e1baa
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-19 06:02:13 -0600

    specials character on python

---
+++ videos/CaracteresEspeciales.py
@@ -0,0 +1,22 @@
+# Caracteres especiales en python
+
+# Salto de linea
+mensaje = "Hola\nMundo"
+print(mensaje)
+
+# Tabulacion
+
+mensaje2 = "\tJuan Perez"
+print(mensaje2)
+
+#Cadena dentro de comillas
+mensaje3 = "Ella dijo \"Hola a todos\""
+print(mensaje3)
+
+#Barra invertida
+mensaje4 = "C:\\Usuarios\\Marce\\Documentos"
+print(mensaje4)
+
+#cadenas crudas
+mensaje5 = r"C:\Usuarios\Marce\Documentos"
+print(mensaje5)
