e99e42622dd996328cbc5ec7c8de19c07d44ec6b
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-21 22:10:34 -0600

    funcion replace

---
+++ videos/Reemplazar.py
@@ -0,0 +1,8 @@
+# Es el uso de .replace(texto viejo, texto nuevo)
+mensaje = "Hola Mundo, Mundo"
+nuevo = mensaje.replace("Mundo", "Python")
+print(nuevo)
+
+# Reemplazar solo una vez
+uno_solo = mensaje.replace("Python", "Dev", 1)
+print(uno_solo)
