933d741b3ae977020818c0ebf069c577bbda6dca
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-23 22:10:56 -0600

    learning more about inputs

---
+++ videos/SistemaEmpleados.py
@@ -0,0 +1,15 @@
+print("*** Sistema de Empleados ***")
+nombre_empleado = input("Ingrese el nombre del empleado: ")
+edad_empleado = int(input("Ingrese la edad del empleado: "))
+salario_empleado = float(input("Ingrese el salario del empleado: "))
+es_jefe_departamento = input("Es jefe de departamento (Si/No)?")
+
+#convertir la variable es_jefe_departamento
+es_jefe_departamento = es_jefe_departamento.lower() == "si"
+
+# Datos que imprimir
+print("\nDatos del empleado")
+print(f"Nombre: {nombre_empleado}")
+print(f"Edad: {edad_empleado}")
+print(f"Salario: {salario_empleado:.2f}")
+print(f"Es jefe de departamento? {es_jefe_departamento}")
