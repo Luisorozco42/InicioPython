00cf4ac8816759c91404bc28355b5d9d451291ab
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-28 18:22:21 -0600

    operators

---
+++ videos/Operadores.py
@@ -0,0 +1,33 @@
+# Vamos a ver primeramente los operadores matemáticos para realizar código
+
+# Operadores aritméticos
+a = 10
+b = 3
+
+# Suma
+suma = a + b
+print(f"Suma: {suma}")
+
+# Resta
+resta = a - b
+print(f"Resta: {resta}")
+
+# Multiplication
+multiplicacion = a * b
+print(f"Multiplicación: {multiplicacion}")
+
+# División entera
+division = a / b
+print(f"División: {division}")
+
+# Division entera, donde queda residuo
+division_entera = a // b
+print(f"División entera: {division_entera}")
+
+# Módulo % residuo de la división
+modulo = a % b
+print(f"Residuo de la división: {modulo}")
+
+# Exopente o potencia
+exponente = a ** b
+print(f"Exponente: {exponente}")
