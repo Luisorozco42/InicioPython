71f607f50d40cb4f79eab3811b0d7678c8b7f435
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-29 16:51:48 -0600

    or operator

--- videos/OperadoresLogicos.py
+++ videos/OperadoresLogicos.py
@@ -1,6 +1,11 @@
 # Vamos a ver los distintos tipos de operadores lógicos
 # Operador and (&& en otros lenguajes)
 print(f"*** Operadores Lógicos ***")
-condicion1 = condicion2 = False
+condicion1 = False
+condicion2 = True
 resultado1 = condicion1 and condicion2
-print(f"Resultado de {condicion1} and {condicion2}: {resultado1}")
+print(f"Resultado de {condicion1} and {condicion2} es: {resultado1}")
+
+# Operador OR (|| en otros lenguajes)
+resultado2 = condicion1 or condicion2
+print(f"Resultado de {condicion1} or {condicion2} es: {resultado2}")
