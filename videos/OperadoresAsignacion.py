193ef0446bb29e31c6c2f846c86930fe55653777
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-28 18:37:57 -0600

    ways to asignate values

---
+++ videos/OperadoresAsignacion.py
@@ -0,0 +1,17 @@
+# Toca ver ahora los operadores de asignación
+print("*** Operadores de Asignación ***")
+numero = 5
+print(f"Valor de numero: {numero}")
+numero = 10
+print(f"Valor de numero: {numero}")
+cadena = 'Saludos desde Python'
+print(f"Valor de cadena: {cadena}")
+
+# Asignación múltiple
+x, y, z = 5, 'Hola', -9.15
+print(f"Valor de x = {x}, y = {y}, z = {z}")
+
+# Asignación encadenada (todas las variables van a tener el mismo valor inicial)
+a = b = c = 10
+print(f"Valor de a = {a}, b = {b}, c = {c}")
+
