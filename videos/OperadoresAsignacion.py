42ddde0f84b7d6f80c894b8b77b7bf1d198912c8
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-28 18:54:51 -0600

    multiple asign

--- videos/OperadoresAsignacion.py
+++ videos/OperadoresAsignacion.py
@@ -15,3 +15,13 @@ print(f"Valor de x = {x}, y = {y}, z = {z}")
 a = b = c = 10
 print(f"Valor de a = {a}, b = {b}, c = {c}")
 
+# Intercambio de valores de una variable sin necesidad de una variable temporal
+x, y = 5, 10
+print(f"Valores iniciales x = {x}, y = {y}")
+# Aplicación de asignación múltiple, intercambiamos valores
+x, y = y, x
+print(f"Valores intercambiados x = {x}, y = {y}")
+
+# Recibir múltiples valores de la entrada del usuario
+nombre, apellido = input('Ingresa tu nombre y apellido separados por una coma: ').split(',')
+print(f"Nombre: {nombre.strip()}, Apellido: {apellido.strip()}")
