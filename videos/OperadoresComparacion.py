1c5e9eb0c73b58713ec6c226a024a6f9ae64c9f3
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-28 19:19:36 -0600

    learning about comparison operators

---
+++ videos/OperadoresComparacion.py
@@ -0,0 +1,28 @@
+# Estos también se conocen como operadores racionales
+print("*** Operadores de comparación (Relacionales) ***")
+a, b = 7, 5
+print(f"Valor inicial: {a}, b: {b}")
+
+# Operador de igualdad
+resultado = a == b
+print(f"Resultado de evaluar a == b es: {resultado}")
+
+# Operador diferente
+resultado = a != b
+print(f"Resultado de evaluar a != b es: {resultado}")
+
+# Operador de mayor que
+resultado = a > b
+print(f"Resultado de evaluar a > b es: {resultado}")
+
+# Operador de mayor igual que
+resultado = a >= b
+print(f"Resultado de evaluar a >= b es: {resultado}")
+
+# Operador de menor que
+resultado = a < b
+print(f"Resultado de evaluar a < b es: {resultado}")
+
+# Operador menor igual que
+resultado = a <= b
+print(f"Resultado de evaluar a <= b es: {resultado}")
