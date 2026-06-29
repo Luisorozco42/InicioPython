42a65880c79811b42d30058eaf3797f2a067c540
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-29 16:39:32 -0600

    and condition

---
+++ videos/OperadoresLogicos.py
@@ -0,0 +1,6 @@
+# Vamos a ver los distintos tipos de operadores lógicos
+# Operador and (&& en otros lenguajes)
+print(f"*** Operadores Lógicos ***")
+condicion1 = condicion2 = False
+resultado1 = condicion1 and condicion2
+print(f"Resultado de {condicion1} and {condicion2}: {resultado1}")
