3cc6cdaefb0c1106e04d7d7842c93b135de236d9
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-01 17:16:46 -0600

    health and fitness

---
+++ videos/SaludFitness.py
@@ -0,0 +1,19 @@
+print("*** Aplicacion Salud y Fitness ***")
+
+#Constantes
+META_PASOS_DIARIOS = 10000
+CALORIAS_POR_PASO = 0.04 #Valor aprox, en kilocalorias
+
+nombre_usuario = input("Cual es tu nombre?\n")
+pasos_diarios = int(input("Cuantos pasos has caminado hoy?\n"))
+
+meta_alcanzada = pasos_diarios >= META_PASOS_DIARIOS
+meta_alcanzada_txt = 'Sí' if meta_alcanzada else 'No'
+# Calculamos las calorias quemadas
+calorias_quemadas = pasos_diarios * CALORIAS_POR_PASO
+
+print(f"\nUsuario: {nombre_usuario}")
+print(f"Pasos dados hoy: {pasos_diarios}")
+print(f"Calorias quemadas: {calorias_quemadas}")
+print(f"Meta de pasos diario alcanzada: {meta_alcanzada_txt}")
+print(f"La meta de pasos diarios es: {META_PASOS_DIARIOS} pasos")
