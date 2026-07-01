71aa96afdf30780467d24cc22588a78f1b5c4d9a
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-01 17:04:03 -0600

    ternary operator

---
+++ videos/OperadorTernario.py
@@ -0,0 +1,7 @@
+# Bueno a ver como es el ternario en python, honestamente es mucho más fácil aquí xd
+print("*** Operador ternario ***")
+
+edad = int(input("Cual es tu edad?\n"))
+
+es_adulto = 'Si' if edad >= 18 else 'No'
+print(f"Es un adulto? {es_adulto}")
