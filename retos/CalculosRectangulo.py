b7060048f08f7d357476395185f2d66f801ae95f
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-01 15:33:18 -0600

    challenge #9

---
+++ retos/CalculosRectangulo.py
@@ -0,0 +1,12 @@
+# En este reto toca calcular el área de un rectángulo y su perímetro
+# Área = base * altura
+# Perimetro = 2 * (base + altura)
+
+print("*** sistema de Cálculos del Area y Perímetro de un Rectángulo ***")
+base = float(input("Ingrese el valor de la base: "))
+altura = float(input("Ingrese el valor de la altura: "))
+
+area = base * altura
+perimetro = 2 * (base + altura)
+
+print(f"El area del rectanngulo es de: {area} y  el perimetro es de: {perimetro}")
