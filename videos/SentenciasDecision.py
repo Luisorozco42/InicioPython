003779353910cd0c806aa7c69edf9b263313652d
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-01 16:07:06 -0600

    learning about if else and elif

---
+++ videos/SentenciasDecision.py
@@ -0,0 +1,13 @@
+# El primer video sobre esto es una explicación y el segundo son diagramas de flujo
+# El 3ro es un ejemplo xd y el 4to es para el else xd y el 5to es sobre el elif (conocido como else if para la pana)
+
+print("*** Sentencia If ***")
+
+edad = 16
+
+if edad >= 18:
+    print(f"Eres mayor de edad. Tienes {edad} años")
+elif 13 <= edad < 18:
+    print(f"Eres un adolescente. Tienes {edad} años")
+else:
+    print(f"Eres menor de edad. Tienes {edad} años")
