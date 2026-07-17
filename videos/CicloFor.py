bfd9a890bcff9b57465a0ef5f4859343aadd90bc
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-17 17:53:41 -0600

    for loop

---
+++ videos/CicloFor.py
@@ -0,0 +1,13 @@
+print("*** Ciclo For ***")
+
+cadena = "Hola Mundo"
+
+print("Recorremos los caracteres de una cadena:")
+for letra in cadena:
+    print(letra, end=" ")
+
+print("\nRecorremos la lista de frutas:")
+
+frutas = ["Plátano", "Fresa", "Mango"]
+for fruta in frutas:
+    print(fruta, end=" ")
