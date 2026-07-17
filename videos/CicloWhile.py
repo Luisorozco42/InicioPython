db2523fb6cc5f6233baa1a23335135b5750c9d11
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-17 17:47:07 -0600

    while loop

---
+++ videos/CicloWhile.py
@@ -0,0 +1,7 @@
+print("*** Ciclo While ***")
+
+contador  = 1
+
+while contador <= 5:
+    print(contador, end=" ") # Esto de end es nuevo para mi, sirve para dar un final distinto al salto de linea
+    contador += 1
