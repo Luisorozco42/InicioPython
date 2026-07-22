ae8e26c43264987baab725eebff6767a2a0893ae
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-21 21:22:00 -0600

    challenge #19

---
+++ retos/CreacionValidacionPassword.py
@@ -0,0 +1,10 @@
+# Crear un programa que permita al usuario crear una contraseña y ser validada con cierto requisito
+# El requisito es que esta sea mayor igual a 6 caracteres
+
+print("*** Sistema de creación y validación de contraseña ***")
+password = input("Ingrese una contraseña de 6 caracteres o más\n")
+
+while password == "" or len(password) <= 6:
+    password = input("Por favor corrija el largo de la contraseña\n")
+else:
+    print("Su contraseña ha sido creada correctamente. Enhorabuena!")
