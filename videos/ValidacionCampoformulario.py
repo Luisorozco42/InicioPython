6d9420a3e230ecc94327fac148679def9c654bee
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-22 19:29:41 -0600

    learning how to validate data

---
+++ videos/ValidacionCampoformulario.py
@@ -0,0 +1,8 @@
+print("*** Validación Campo de un Formulario ***")
+
+nombre_usuario = None
+
+while not nombre_usuario:
+    nombre_usuario: input("Ingresa tu nombre de usuario: ")
+else:
+    print(f"Nombre de usuario válido: {nombre_usuario}")
