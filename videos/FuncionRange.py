3157801752570d73f2ae6574a108db1aff51f87b
Luisorozco42 <lorozcolazo@gmail.com>
2026-07-22 19:37:06 -0600

    function range

---
+++ videos/FuncionRange.py
@@ -0,0 +1,15 @@
+print("*** Funcion Range ***")
+print("Secuencia del 0 al 4")
+
+for i in range(5): #fin = 5 -1
+    print(i, end=" ")
+
+#Range(inicio (opcional), fin, incremento (opcional))
+print("\n\nSecuencia del 10 al 20")
+for i in range(10, 21):
+    print(i, end=" ")
+
+print("\n\nSecuencia del 20 al 30 de 2 en 2")
+
+for i in range(20, 31, 2):
+    print(i, end=" ")
